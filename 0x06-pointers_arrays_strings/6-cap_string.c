637d784d9f22cdced5a5e7f83fde7e7b91d71d8c
TOMIWA00 <olatomiwaquadri@gmail.com>
2022-12-22 20:58:54 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,32 +5,34 @@
  * @str: string
  * Return: pointer to string
  */
-char *cap_string(char *str)
+char *cap_string(char *)
 {
 	int i = 0;
 
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
+
 			i++;
 		if (str[i - 1] == ' ' ||
-			 str[i - 1] == '\t' ||
-			 str[i - 1] == '\n' ||
-			  str[i - 1] == ',' ||
-			  str[i - 1] == ';' ||
-			  str[i - 1] == '.' ||
-			  str[i - 1] == '!' ||
-			  str[i - 1] == '?' ||
-			  str[i - 1] == '"' ||
-			  str[i - 1] == '(' ||
-			  str[i - 1] == ')' ||
-			  str[i - 1] == '{' ||
-			  str[i - 1] == '}' ||
-			  i == 0)
-			str[i] -= 32;
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' ||
+			str[i - 1] == ';' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == '!' ||
+			str[i - 1] == '?' ||
+			str[i - 1] == '"' ||
+			str[i - 1] == '(' ||
+			str[i - 1] == ')' ||
+			str[i - 1] == '{' ||
+			str[i - 1] == '}' ||
+			i == 0)
+			   str[i] -= 32;
 
 		i++;
 	}
 
 	return (str);
 }
+
